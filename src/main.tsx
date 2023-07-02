ba0bf5d291bb7febcc6c5fe60a87856acf0f3e73
JUNIgroup <devel@junigroup.de>
2023-07-02 19:34:47 +0200

    feat: implement IdbCacheDatabase
    Use an IDB store to cache the data from inner database,
    usually the remote database, like firestore database.

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,21 @@ import { BrowserRouter } from 'react-router-dom'
 import { IDB_ID } from './app.constants'
 import { createStore } from './business/app.store'
 import { FirebaseRestAuthService } from './business/auth/service/firebase-rest-auth-service'
+import { FirestoreDatabase } from './business/recipe-books/database/firestore/firestore-database'
+import { FirestoreService } from './business/recipe-books/database/firestore/firestore-service.api'
+import { IdbCacheDatabase } from './business/recipe-books/database/idb-cache/idb-cache-database'
 import { IdbService } from './infrastructure/database/idb/idb.service'
 import { dbUpgrades, dbVersion } from './infrastructure/database/idb/idb.upgrades'
 import { memoryPersistence, storagePersistence } from './infrastructure/firebase/persistence'
 import { RestAuthService } from './infrastructure/firebase/rest-auth-service'
-import { App } from './presentation/app'
-import { createConsoleLogger, createDebugObserver } from './utilities/logger'
-
-import { FirestoreDatabase } from './business/recipe-books/database/firestore/firestore-database'
-import { FirestoreService } from './business/recipe-books/database/firestore/firestore-service.api'
-import './index.scss'
 import {
   createFirestoreRestServiceForEmulator,
   createFirestoreRestServiceForRemote,
 } from './infrastructure/firestore/firestore-rest-factory'
+import { App } from './presentation/app'
+import { createConsoleLogger, createDebugObserver } from './utilities/logger'
+
+import './index.scss'
 
 type LogScope = 'app' | 'utils' | 'infra' | 'business' | 'ui'
 
@@ -50,7 +51,12 @@ const firestoreOptions = {
 const firestoreService: FirestoreService = import.meta.env.VITE_FIREBASE__USE_EMULATOR
   ? createFirestoreRestServiceForEmulator(logger, firestoreOptions)
   : createFirestoreRestServiceForRemote(logger, firestoreOptions)
-const database = new FirestoreDatabase(logger, firestoreService)
+const firestoreDatabase = new FirestoreDatabase(logger, firestoreService)
+const cacheDatabase = new IdbCacheDatabase(logger, firestoreDatabase, {
+  cacheName: 'de.junigroup.app.recipebook',
+  clearOnStart: true,
+})
+const database = cacheDatabase
 
 const store = createStore({
   storage,
