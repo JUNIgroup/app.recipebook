e4c8d8600e45c0d6e867b9e2abd45dbdbb6be8d0
JUNIgroup <devel@junigroup.de>
2023-09-05 17:49:52 +0200

    chore: delete react/redux components (unused)

--- src/utilities/redux/draft.ts
+++
@@ -1,7 +0,0 @@
-import { Draft } from '@reduxjs/toolkit'
-
-export function castDraft<T>(value: T): Draft<T> {
-  return value as Draft<T>
-}
-
-export type WritableDraft<T> = { -readonly [K in keyof T]: Draft<T[K]> }
